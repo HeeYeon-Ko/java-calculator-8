4b2d1ff435ea14aafa2ef93ac44fe26e2709c77d
HeeYeon-Ko <rhgmldus6@gmail.com>
2025-10-17 18:38:33 +0900

    feat: 기본 구분자(쉼표, 콜론)로 숫자 더하기 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,35 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        if(input.isEmpty()){
+            System.out.println("결과 : 0");
+            return;
+        }
+
+        String[] nums = input.split("[,:]");
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            try{
+                int num = Integer.parseInt(nums[i]);
+
+                if(num < 0){
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                }
+
+                result += num;
+            } catch(NumberFormatException e){
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
+
+        System.out.println("결과 : " + result);
     }
 }
