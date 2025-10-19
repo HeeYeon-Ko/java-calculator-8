96a1dab54c85a2e1a30ac1ac99c2ccb92d101ff0
HeeYeon-Ko <rhgmldus6@gmail.com>
2025-10-20 01:40:38 +0900

    refactor: 전역 변수 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,6 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    static int result = 0; // 결과
-    static String[] nums; // 추출한 숫자
-
     public static void main(String[] args) {
 
         // TODO: 프로그램 구현
@@ -18,40 +15,38 @@ public class Application {
             return;
         }
 
+        int result; // 결과
+        String[] nums; // 추출한 숫자
+
         // 커스텀 구분자 지정시
         if (input.startsWith("//")) {
             String[] customParts = input.split("\\\\n");
             String customDelimiter = customParts[0].substring(2);
             String numberPart = customParts[1];
-
             nums = numberPart.split(customDelimiter);
-
-            calculate(nums);
-
         } else { // 기본 구분자
             nums = input.split("[,:]");
-
-            calculate(nums);
         }
 
-        // 결과 출력
+        result = calculate(nums);
         System.out.println("결과 : " + result);
     }
 
     // 더하기 계산
-    private static void calculate(String[] arr) {
+    private static int calculate(String[] arr) {
+        int sum = 0;
+
         for (String s : arr) {
             try {
                 int num = Integer.parseInt(s);
-
                 if (num < 0) {
                     throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
                 }
-
-                result += num;
+                sum += num;
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
             }
         }
+        return sum;
     }
 }
